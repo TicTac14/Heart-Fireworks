6454e38e164bb23f2a6b7b3956a9c347c5051c6e
TicTac14 <francisco.mendez114@gmail.com>
2023-10-22 19:06:29 -0400

    Heart Addition

--- firework.js
+++ firework.js
@@ -37,14 +37,31 @@ class Firework {
 
 
     explode(){
-        const particleAmount = floor(random(10, 20));
         let particles = [];
 
-        for (let i = 0; i < particleAmount; i++){
-            let vect = p5.Vector.random2D();
-            vect.mult(this.power);
-            particles[i] = new Particle(this.particles[0].pos.x, this.particles[0].pos.y, vect, this.color, this.size);
+        if (floor(random(100)) <= 15){
+            let particleAmount = 30
+            for (let i = 0; i < particleAmount; i++){
+                let theta = (TWO_PI/particleAmount)*i;
+                let r = 5;
+                let x = r*16 * Math.pow(Math.sin(theta), 3);
+                let y = -r*(13*Math.cos(theta)-5*Math.cos(2*theta)
+                            -2*Math.cos(3*theta)-Math.cos(4*theta));
+                let t = createVector(this.particles[0].pos.x, this.particles[0].pos.y);
+                particles.push(
+                    new Particle(x + t.x, y + t.y, createVector(), this.color, this.size)
+                );
+            }
+        }else {
+            let particleAmount = floor(random(10, 20));
+            for (let i = 0; i < particleAmount; i++){
+                let vect = p5.Vector.random2D();
+                vect.mult(this.power);
+                particles[i] = new Particle(this.particles[0].pos.x, this.particles[0].pos.y, vect, this.color, this.size);
+            }
         }
+        
+
         this.particles = particles;
 
     }
